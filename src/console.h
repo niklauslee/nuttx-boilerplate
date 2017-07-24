d8f737bcd1c6d81109ec57e50856f5be2b1dfd6e
Minkyu Lee <niklaus.lee@gmail.com>
2017-07-24 15:22:54 +0900

    header update

--- src/console.h
+++ src/console.h
@@ -1,1 +1,3 @@
 #define COMMAND_LENGTH_MAX 255
+
+void console_put_char(char ch);
---
+++ src/hardware.h
@@ -0,0 +1,15 @@
+
+void hardware_initialize ();
+void hardware_finalize ();
+
+long hardware_get_serial_number();
+long hardware_get_system_time();
+
+// i2c_setup
+// i2c_write
+// i2c_read
+
+// spi
+// serial
+// timer
+// ...
