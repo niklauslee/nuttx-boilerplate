42268972b8afaae17406134a155bde6093966157
niklaus <niklaus.lee@gmail.com>
2017-08-22 20:56:29 +0900

    Change board by STM32F411E-Disco and settings for USB CDC/ACM console

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
+# Overview
+
+* Board: STM32F411E-Discovery
+* Use USB CDC/ACM for console (not USART2)
+
+# Development settings
+
+Required packages:
+* __GCC ARM__
+* __CMake__ : https://cmake.org/
+* __kconfig-frontends__
+
+```sh
+$ git clone https://github.com/jameswalmsley/kconfig-frontends.git
+$ cd kconfig-frontends
+$ ./bootstrap
+$ ./configure --enable-mconf
+$ make
+$ sudo make install
+$ sudo ldconfig
+```
+
+* __St-Util__ for Flash
+
 # Build
 
 1. Checkout repository
@@ -13,23 +37,41 @@ $ git submodule update
 
 ```sh
 $ cd kameleon-firmware/deps/nuttx/tools
-$ ./configure.sh -a ../../src nucleo-f4x1re/f411-nsh
-$ cd .. # at kameleon-firmware/deps/nuttx
-$ make menuconfig
+$ ./configure.sh -a ../../src stm32f411e-disco/nsh
+$ cd ..
 ```
+`.config` and `Make.defs` are created at `kameleon-firmware/deps/nuttx`
 
 3. Change Configuration Variables
 
+```sh
+# at kameleon-firmware/deps/nuttx
+$ make menuconfig
+```
+
+* `System Type`
+  * `STM32 Peripheral Support`
+    * `OTG FS` : __Check__
+* `Board Selection`
+  * `Enable boardctl() interface` : __Check__
+    * `Enable USB device controls` : __Check__
 * `RTOS Features`
   * `Tasks and Scheduling`
     * `Application entry point` : __kameleon_main__
+  * `Files and I/O`
+    * `Enable /dev/console` : __Uncheck__
+* `Device Drivers`
+  * `Serial Driver Support`
+    * `Serial console` : __No serial console__
+  * `USB Device Driver Support` : __Check__
+    * `USB Modem (CDC/ACM) support` : __Check__
+      * `CDC/ACM console device` : __Check__
+      * `Receive buffer size` : __256__
+      * `Transmit buffer size` : __256__
 * `Binary Loader`
   * `Disable BINFMT support` : __Check__
 * `Library Routines`
   * `Have C++ compiler` : __Uncheck__
-* `Application Configuration`
-  * `Platform-specific Support`
-    * `Have C++ initialization` : __Uncheck__
 
 3. Make
 
@@ -41,4 +83,16 @@ $ make
 
 ```sh
 $ st-flash write nuttx.bin 0x8000000
+```
+
+5. Terminal
+
+```sh
+# Press RESET button on board
+$ screen /dev/tty.usbmodem1 115200 # macOS
+# or
+$ screen /dev/ttyACM0 115200 # Linux
+
+# Hit return several times,
+# then "Hello, World" will be printed.
 ```
--- src/kameleon_main.c
+++ src/kameleon_main.c
@@ -39,10 +39,185 @@
 
 #include <nuttx/config.h>
 #include <stdio.h>
+#include <fcntl.h>
+
+#include <sys/boardctl.h>
+
+#ifdef CONFIG_CDCACM
+#  include <nuttx/usb/cdcacm.h>
+#endif
 
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
+
+ static void nsh_configstdio(int fd)
+ {
+   /* Make sure the stdin, stdout, and stderr are closed */
+
+   (void)fclose(stdin);
+   (void)fclose(stdout);
+   (void)fclose(stderr);
+
+   /* Dup the fd to create standard fd 0-2 */
+
+   (void)dup2(fd, 0);
+   (void)dup2(fd, 1);
+   (void)dup2(fd, 2);
+
+   /* fdopen to get the stdin, stdout and stderr streams. The following logic depends
+    * on the fact that the library layer will allocate FILEs in order.  And since
+    * we closed stdin, stdout, and stderr above, that is what we should get.
+    *
+    * fd = 0 is stdin  (read-only)
+    * fd = 1 is stdout (write-only, append)
+    * fd = 2 is stderr (write-only, append)
+    */
+
+   (void)fdopen(0, "r");
+   (void)fdopen(1, "a");
+   (void)fdopen(2, "a");
+ }
+
+static int nsh_nullstdio(void)
+{
+ int fd;
+
+ /* Open /dev/null for read/write access */
+
+ fd = open("/dev/null", O_RDWR);
+ if (fd >= 0)
+   {
+     /* Configure standard I/O to use /dev/null */
+
+     nsh_configstdio(fd);
+
+     /* We can close the original file descriptor now (unless it was one of
+      * 0-2)
+      */
+
+     if (fd > 2)
+      {
+         close(fd);
+      }
+
+     return OK;
+   }
+
+ return fd;
+}
+
+int board_app_initialize(uintptr_t arg)
+{
+#ifndef CONFIG_BOARD_INITIALIZE
+  /* Perform board-specific initialization */
+
+  return stm32_bringup();
+#else
+  return OK;
+#endif
+}
+
+#if defined(CONFIG_CDCACM)
+#  define CONFIG_NSH_USBCONDEV "/dev/ttyACM0"
+#elif defined(CONFIG_PL2303)
+#  define CONFIG_NSH_USBCONDEV "/dev/ttyUSB0"
+#else
+#  define CONFIG_NSH_USBCONDEV "/dev/console"
+#endif
+
+static int nsh_waitusbready(void)
+{
+  char inch;
+  ssize_t nbytes;
+  int nlc;
+  int fd;
+
+  /* Don't start the NSH console until the console device is ready.  Chances
+   * are, we get here with no functional console.  The USB console will not
+   * be available until the device is connected to the host and until the
+   * host-side application opens the connection.
+   */
+
+restart:
+
+  /* Open the USB serial device for read/write access */
+
+  do
+    {
+      /* Try to open the console */
+
+      fd = open(CONFIG_NSH_USBCONDEV, O_RDWR);
+      if (fd < 0)
+        {
+          /* ENOTCONN means that the USB device is not yet connected. Anything
+           * else is bad.
+           */
+
+          DEBUGASSERT(errno == ENOTCONN);
+
+          /* Sleep a bit and try again */
+
+          sleep(2);
+        }
+    }
+  while (fd < 0);
+
+  /* Now wait until we successfully read a carriage return a few times.
+   * That is a sure way of know that there is something at the other end of
+   * the USB serial connection that is ready to talk with us.  The user needs
+   * to hit ENTER a few times to get things started.
+   */
+
+  nlc = 0;
+  do
+    {
+      /* Read one byte */
+
+      inch = 0;
+      nbytes = read(fd, &inch, 1);
+
+      /* Is it a carriage return (or maybe a newline)? */
+
+      if (nbytes == 1 && (inch == '\n' || inch == '\r'))
+        {
+          /* Yes.. increment the count */
+
+          nlc++;
+        }
+      else
+        {
+          /* No.. Reset the count.  We need to see 3 in a row to continue. */
+
+          nlc = 0;
+
+          /* If a read error occurred (nbytes < 0) or an end-of-file was
+           * encountered (nbytes == 0), then close the driver and start
+           * over.
+           */
+
+          if (nbytes <= 0)
+            {
+              (void)close(fd);
+              goto restart;
+            }
+        }
+    }
+  while (nlc < 3);
+
+  /* Configure standard I/O */
+
+  nsh_configstdio(fd);
+
+  /* We can close the original file descriptor now (unless it was one of 0-2) */
+
+  if (fd > 2)
+    {
+      close(fd);
+    }
+
+  return OK;
+}
 
 /****************************************************************************
  * hello_main
@@ -54,6 +229,33 @@ int main(int argc, FAR char *argv[])
 int kameleon_main(int argc, char *argv[])
 #endif
 {
+  // FAR struct console_stdio_s *pstate = nsh_newconsole();
+  struct boardioc_usbdev_ctrl_s ctrl;
+  FAR void *handle;
+  int ret;
+
+  /* Initialize the USB serial driver */
+#if defined(CONFIG_PL2303) || defined(CONFIG_CDCACM)
+
+#ifdef CONFIG_CDCACM
+  ctrl.usbdev   = BOARDIOC_USBDEV_CDCACM;
+  ctrl.action   = BOARDIOC_USBDEV_CONNECT;
+  ctrl.instance = 0;
+  ctrl.handle   = &handle;
+#endif
+
+  ret = boardctl(BOARDIOC_USBDEV_CONTROL, (uintptr_t)&ctrl);
+  UNUSED(ret); /* Eliminate warning if not used */
+  DEBUGASSERT(ret == OK);
+#endif
+
+  /* Configure to use /dev/null if we do not have a valid console. */
+
+#ifndef CONFIG_DEV_CONSOLE
+  (void)nsh_nullstdio();
+#endif
+
+  ret = nsh_waitusbready();
   printf("Hello, World!!\n");
   return 0;
 }
